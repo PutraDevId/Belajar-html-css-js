f5d6c2cbc17eb62687a720c9b4830e146691a0a6
Muhammad Saputra <putradav212@gmail.com>
2022-11-27 16:48:26 +0700

    new lerning

--- projeck wab/wab 1/index.js
+++ projeck wab/wab 1/index.js
@@ -1,30 +1,6 @@
-const kdwResistor = {
-    coklat: "1",
-    merah: "2",
-    oren: "3",
-    kuning: "4",
-    hijau: "5",
-    biru: "6",
-    ungu: "7",
-    abu: "8",
-    putih: "9"
-};
+const a;
+const b;
 
-const g3Resistor = {
-    coklat: "0",
-    merah: "00",
-    oren: "000",
-    kuning: "0000",
-    hijau: "00000",
-    biru: "000000",
-    ungu: "0000000",
-    abu: "00000000",
-    putih: "000000000"
-};
-
-const gelang1 = document.getElementsByClassName("gelang No 1");
-gelang1[0].style.color = "blue";
-
-for (i = 0; i < g3Resistor; i++) {
-    print("0");
-}
+if (i = 0; i <= 1; i++) {
+    document.writeln( < h2 > hallo < /h2>)
+    }
